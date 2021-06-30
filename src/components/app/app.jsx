b7a92dceae04967d9c75a28914efa9e84fc25785
OliviaTa <ir.klypina@gmail.com>
2021-06-30 14:40:49 +0800

    connected to API

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,19 +1,37 @@
 import React from 'react';
-import burgersData from '../../utils/data';
 import AppHeader from '../app-header/app-header';
 import BurgerConstructor from '../burger-constructor/burger-constructor';
 import BurgerIngredients from '../burger-ingredients/burger-ingredients';
 import styles from './app.module.css';
 
+const INGREDIENTS_URL = 'https://norma.nomoreparties.space/api/ingredients';
+
 function App() {
-  const data = burgersData.map(item => ({ ...item, count: 1 }));
+
+  const [burgersData, setBurgersData] = React.useState([]);
+
+  React.useEffect(() => {
+    const getData = async () => {
+      try {
+        const res = await fetch(INGREDIENTS_URL);
+        const data = await res.json();
+        setBurgersData(
+          data.data.map(item => ({ ...item, count: 1 }))
+        );
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    getData();
+  }, []);
 
   return (
     <div className={`${styles.app} text text_type_main-default`}>
       <AppHeader />
       <main className={styles.main}>
-        <BurgerIngredients data={data} />
-        <BurgerConstructor data={data.filter(element => element.count > 0)} />
+        <BurgerIngredients data={burgersData} />
+        <BurgerConstructor data={burgersData.filter(element => element.count > 0)} />
       </main>
     </div>
   );
