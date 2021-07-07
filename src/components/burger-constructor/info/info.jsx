8369cd3c49499657917d596dd44d75a0151b283b
OliviaTa <ir.klypina@gmail.com>
2021-07-07 17:05:59 +0800

    get order details from api

--- src/components/burger-constructor/info/info.jsx
+++ src/components/burger-constructor/info/info.jsx
@@ -9,7 +9,42 @@ function Info() {
     const { constructorState } = React.useContext(ConstructorContext);
 
     const [isModalOpen, setIsModalOpen] = React.useState(false);
-    const [orderNumber, setOrderNumber] = React.useState('034536');
+    const [orderNumber, setOrderNumber] = React.useState('');
+
+    const onButtonClick = () => {
+        const burgersIdList = constructorState.burgersData.filter(item => item.count > 0).map(item => item._id);
+
+        const getOrderData = async () => {
+            try {
+                const res = await fetch('https://norma.nomoreparties.space/api/orders', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        ingredients: burgersIdList
+                    })
+                });
+
+                if (res.ok) {
+                    const data = await res.json();
+                    if (data.success) {
+                        setOrderNumber(data.order.number);
+                    } else {
+                        Promise.reject(`Ошибка ${data.message}`);
+                    }
+                } else {
+                    Promise.reject(`Ошибка ${res.status}`)
+                }
+            } catch (err) {
+                console.log(err);
+            }
+        };
+
+        getOrderData();
+
+        setIsModalOpen(true);
+    };
 
     return (
         <div className={styles.info} id='info'>
@@ -20,7 +55,7 @@ function Info() {
             <Button
                 type="primary"
                 size="large"
-                onClick={() => setIsModalOpen(true)}
+                onClick={onButtonClick}
             >
                 Оформить заказ
             </Button>
