9cd6e2bb42ae944bed58792c8b53df8ed4ec1561
OliviaTa <ir.klypina@gmail.com>
2021-07-14 18:22:34 +0800

    prepare state

---
+++ src/services/reducers/burger-constructor.js
@@ -0,0 +1,10 @@
+const initialState = {
+    ingredients: [],
+    constructorIngredients: [],
+    currentIngredient: {},
+    order: {}
+};
+
+export const burgerConstructorReducer = (state = initialState, action) => {
+    return state;
+};
