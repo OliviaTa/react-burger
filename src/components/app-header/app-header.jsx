2d45fe2505eb0f6344b6db1020606eceed577dfd
OliviaTa <ir.klypina@gmail.com>
2021-08-19 19:09:12 +0800

    route to home page by logo click

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -2,13 +2,16 @@ import { Logo } from '@ya.praktikum/react-developer-burger-ui-components';
 import NavList from './nav-list/nav-list';
 import ProfileMenu from './profile-menu/profile-menu';
 import styles from './app-header.module.css';
+import { Link } from 'react-router-dom';
 
 function AppHeader() {
     return (
         <header className={`${styles.appHeader} pt-4 pb-4`}>
             <nav className={styles.content}>
                 <NavList />
-                <Logo />
+                <Link to='/'>
+                    <Logo />
+                </Link>
                 <ProfileMenu />
             </nav>
         </header>
